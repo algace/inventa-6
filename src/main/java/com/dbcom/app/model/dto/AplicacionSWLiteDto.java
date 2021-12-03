455e1db8945601231215be06718c5259471449f2
NEORIS\jorge.gonzalezm <jorge.gonzalezm@neoris.com>
2021-12-03 12:23:10 +0100

    Nuevo Dto AplicacionSWLiteDto

---
+++ src/main/java/com/dbcom/app/model/dto/AplicacionSWLiteDto.java
@@ -0,0 +1,42 @@
+package com.dbcom.app.model.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AplicacionSWLiteDto {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4263045492135704248L;
+
+	private Long id;
+	
+	@Size(min = 1, max = 50, message = "{validation.min1max50}")
+	private String nombre;
+	 
+	@Size(min = 1, max = 250, message = "{validation.min1max250}")
+	private String archivo;
+	
+	@NotNull(message = "{validation.notNull}")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate fecha;
+	 
+	@NotNull(message = "{validation.notNull}")
+	@DateTimeFormat(pattern = "HH:mm")
+	private LocalTime hora;
+}
